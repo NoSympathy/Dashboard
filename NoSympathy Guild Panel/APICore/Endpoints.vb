24e59014d5dcffb7058533e8d1758eb0b397f2d0
radit <radit@voxteneo.co.id>
2015-12-27 23:36:58 +0700

    engine

---
+++ NoSympathy Guild Panel/APICore/Endpoints.vb
@@ -0,0 +1,13 @@
+﻿Public Class Endpoints
+
+
+    Public Const BaseUrl As String = "https://api.guildwars2.com/"
+    Public Const GuildId As String = "F1503570-D682-E511-A34A-E4115BDF481D"
+
+
+    ''' <summary>
+    ''' guild end points for api
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Const GuildMemberEndPoints As String = "/v2/guild/{0}/members"
+End Class
---
+++ NoSympathy Guild Panel/Models/Guild.vb
@@ -0,0 +1,21 @@
+﻿Imports Newtonsoft.Json
+Public Class Guild
+    Public GuildName As String
+    Public Emblem As String
+    Public Logs As List(Of String)
+    Public Motd As String
+
+    Public Sub New(guildName As String, emblem As String, logs As List(Of String), motd As String)
+
+        Me.Emblem = emblem
+        Me.GuildName = guildName
+        Me.Logs = logs
+        Me.Motd = motd
+
+
+    End Sub
+
+    Public Sub New(json As String)
+        JsonConvert.DeserializeObject(Of Guild)(json)
+    End Sub
+End Class
