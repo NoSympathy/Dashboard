a6fc6c6b44fa54690f7e389e83cd52b195f0c4d3
Michael <crossovver@outlook.com>
2015-12-27 09:19:20 -0500

    Change 1

--- NoSympathy Guild Panel/NoSympathy Guild Panel/Form1.vb
+++ NoSympathy Guild Panel/NoSympathy Guild Panel/Form1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Form1
-
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("http://twitter.com/FragumZ")
+    End Sub
 End Class
