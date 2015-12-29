a60ea204ba2e4de400fda73506bbd2f5555f3a4b
Michael <crossovver@outlook.com>
2015-12-29 09:14:53 -0500

    Ooops, Yo Bahhhd

---
+++ NoSympathy Guild Panel/APIControllers/test.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Net
+
+
+Public Class test
+    Public Function GetCharacters(access_token As String) As String
+        Dim client As WebClient
+        Dim Endpoints As Uri
+        Dim ret As String
+
+        Endpoints = New Uri(APICore.Endpoints.BaseUrl + APICore.Endpoints.CharacterEndPoints + "?access_token=" + access_token)
+
+
+        client = New WebClient()
+        ret = client.DownloadString(Endpoints)
+
+
+        client.Dispose()
+        Return ret
+    End Function
+
+
+
+
+End Class
