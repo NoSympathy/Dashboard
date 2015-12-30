4b660a1e3b31127e3846f8e27e394b07ef8b4122
radit <radit@voxteneo.co.id>
2015-12-31 02:46:34 +0700

    some cleaning in code

--- NoSympathy Guild Panel/APIControllers/GuildController.vb
+++ NoSympathy Guild Panel/APIControllers/GuildController.vb
@@ -5,15 +5,12 @@ Imports Newtonsoft.Json
 Public Class GuildController
     Inherits BaseController
     Public Function GetGuildMembers(access_token As String) As List(Of Member)
-        Dim endPoints As Uri
-        Dim jsonres As String
-        Dim ret As List(Of Member)
+        
+        Dim endPoints = New Uri(String.Format(APICore.Gw2Endpoints.BaseUrl + APICore.Gw2Endpoints.GuildMember, APICore.NosSettings.GuildId, APICore.NosSettings.GuildLeaderAccessToken))
 
-        endPoints = New Uri(String.Format(APICore.Gw2Endpoints.BaseUrl + APICore.Gw2Endpoints.GuildMember, APICore.NosSettings.GuildId, APICore.NosSettings.GuildLeaderAccessToken))
+        Dim jsonres = GetApiDataByUriJson(endPoints)
 
-        jsonres = GetApiDataByUriJson(endPoints)
-
-        ret = JsonConvert.DeserializeObject(Of List(Of Member))(jsonres)
+        Dim ret = JsonConvert.DeserializeObject(Of List(Of Member))(jsonres)
 
 
 
