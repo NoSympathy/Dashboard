b76ffcedc836aab61d1a63b4ce9c9278e69413a6
Michael <crossovver@outlook.com>
2016-01-03 13:26:49 -0500

    Switched GetCharacters Function to GetPVPSTATS

--- NoSympathy Guild Panel/APIControllers/PvPSTATDISPLAY.vb
+++ NoSympathy Guild Panel/APIControllers/PvPSTATDISPLAY.vb
@@ -5,7 +5,7 @@ Imports Models
 
 
 Public Class PvPSTATDISPLAY
-    Public Function GetCharacters(access_token As String) As List(Of PvPSTATDISPLAY)
+    Public Function GetPVPSTATS(access_token As String) As List(Of PvPSTATDISPLAY)
         Dim client As WebClient
         Dim endPoints As Uri
 
