87c687013e1c2d6ca3b26a145754ba5568c25328
Michael <crossovver@outlook.com>
2016-01-08 18:37:18 -0500

    Addons/Guild Roster MGT/Guild Roster MGT.sln has been added and created separately for time being
    Signed-off-by: FragumZ <crossovver@outlook.com>

---
+++ Addons/Guild Roster MGT/Guild Roster MGT/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WebControl1.Source = New Uri("http://nosympathy.net/Guild%20Roster")
+    End Sub
+End Class
