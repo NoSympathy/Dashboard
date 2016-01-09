1f9eb6ae8af412a8f2ccfd1ddd929219801027b2
Mtechnik <FragumZ@users.noreply.github.com>
2016-01-08 22:09:34 -0500

    Delete Form1.vb

--- Addons/Guild Roster MGT/Guild Roster MGT/Form1.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        WebControl1.Source = New Uri("http://nosympathy.net/Guild%20Roster")
-    End Sub
-End Class
